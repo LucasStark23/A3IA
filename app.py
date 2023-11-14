e8ff274c45e9e5685fbae280c93a5337b65757d0
Lucas Paraná Stark <lucaspstark23@gmail.com>
2023-11-14 18:07:53 -0300

    A3 IA 1.0

---
+++ app.py
@@ -0,0 +1,72 @@
+from flask import Flask, jsonify, request
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+import logging
+
+app = Flask(__name__)
+
+
+def carregar_dataset():
+    dataset = pd.read_csv("dataset-hemograma.csv")
+    return dataset
+
+
+dataset_hemograma = carregar_dataset()
+
+y = dataset_hemograma["id"]
+
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(dataset_hemograma.drop(["id", "diagnostico"], axis=1))
+
+model = LogisticRegression()
+model.fit(X_scaled, y)
+
+
+@app.route("/diagnostico", methods=["POST"])
+def sugerir_diagnostico():
+    data = request.get_json(force=True)
+
+    required_fields = [
+        "eritrocitos",
+        "hemoglobina",
+        "hematocrito",
+        "hcm",
+        "vgm",
+        "chgm",
+        "metarrubricitos",
+        "proteina_plasmatica",
+        "leucocitos",
+        "leucograma",
+        "segmentados",
+        "bastonetes",
+        "blastos",
+        "metamielocitos",
+        "mielocitos",
+        "linfocitos",
+        "monocitos",
+        "eosinofilos",
+        "basofilos",
+        "plaquetas",
+    ]
+
+    for field in required_fields:
+        if field not in data:
+            logging.error(f"Campo obrigatório ausente: {field}")
+            return jsonify({"erro": f"O campo {field} é obrigatório."}), 400
+
+    features = [data[field] for field in required_fields]
+    features_scaled = scaler.transform([features])
+
+    diagnostico_predito = model.predict(features_scaled)[0]
+
+    # Converter para um tipo de dado nativo do Python (int)
+    diagnostico_predito = int(diagnostico_predito)
+
+    logging.debug(f"Diagnóstico previsto: {diagnostico_predito}")
+
+    return jsonify({"diagnostico_predito": diagnostico_predito})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
